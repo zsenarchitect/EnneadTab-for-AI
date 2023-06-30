edd4857f0a7d3e56f725b93ab0f900865304c948
zsenarchitect <zsenarchitect@gmail.com>
2023-06-30 18:02:11 -0400

    update

--- scripts/EA_AI_CONVERTER.py
+++ scripts/EA_AI_CONVERTER.py
@@ -35,6 +35,7 @@ try:
     import time
     from playsound import playsound
     import pyautogui
+    print("Loadding Finish.")
 except:
 
     error = traceback.format_exc()
@@ -335,8 +336,8 @@ def is_another_app_running():
         # print window.title
         if window.title == EXE_NAME:
             return True
-        if window.title == "EA_AI_CONVERTER":
-            return True
+        # if window.title == "EA_AI_CONVERTER":
+        #     return True
     return False
 
 
@@ -344,9 +345,42 @@ def is_another_app_running():
 def main():
     if is_another_app_running():
         return
+    
+   
     app = App()
     app.run()
 
 ########################################
 if __name__ == "__main__":
     main()
+
+
+"""
+To to get a icon!!!!!!
+for the app
+
+To upscale images!!!!!!!
+
+
+import requests
+from PIL import Image
+from io import BytesIO
+from diffusers import StableDiffusionUpscalePipeline
+import torch
+
+# load model and scheduler
+model_id = "stabilityai/stable-diffusion-x4-upscaler"
+pipeline = StableDiffusionUpscalePipeline.from_pretrained(
+    model_id, revision="fp16", torch_dtype=torch.float16
+)
+pipeline = pipeline.to("cuda")
+
+# let's download an  image
+url = "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd2-upscale/low_res_cat.png"
+response = requests.get(url)
+low_res_img = Image.open(BytesIO(response.content)).convert("RGB")
+low_res_img = low_res_img.resize((128, 128))
+prompt = "a white cat"
+
+upscaled_image = pipeline(prompt=prompt, image=low_res_img).images[0]
+upscaled_image.save("upsampled_cat.png")"""
